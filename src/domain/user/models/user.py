3ed3260553fe0c1a3ebf685b17b987d4a80ede48
CesarCarriel <cesarcarrieldev@gmail.com>
2021-06-03 11:01:34 -0400

    Feat: include user domain

---
+++ src/domain/user/models/user.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.utils import timezone
+
+
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=60)
+    phone = models.CharField(max_length=20, unique=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)

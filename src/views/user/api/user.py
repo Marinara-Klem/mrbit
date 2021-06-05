2d9717a0fba179dfbe3306ab2c77291726341c60
CesarCarriel <cesarcarrieldev@gmail.com>
2021-06-04 23:24:27 -0400

    Feat: endpoint to add balance manually

--- src/views/user/api/user.py
+++ src/views/user/api/user.py
@@ -1,9 +1,27 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, decorators, response, status
 
 from domain.user.models import User
-from views.user.serializer import UserSerializer
+from views.user.serializer import UserSerializer, UserBalanceCreateSerializer
 
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
+
+    def get_serializer_class(self):
+        if self.action in ['balances']:
+            return UserBalanceCreateSerializer
+        return super().get_serializer_class()
+
+    @decorators.action(methods=['post'], detail=True, url_path='balances')
+    def balances(self, request, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        amount = serializer.validated_data.get('amount')
+
+        user = self.get_object()
+        user.credit_balance(amount=amount)
+        user.save()
+
+        return response.Response({f'${amount} incluido(s) no saldo com sucesso'}, status=status.HTTP_201_CREATED)
--- src/views/user/serializer/user.py
+++ src/views/user/serializer/user.py
@@ -7,3 +7,8 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'first_name', 'last_name', 'email', 'phone')
+
+
+class UserBalanceCreateSerializer(serializers.Serializer):
+    amount = serializers.DecimalField(help_text='amount in dollars(USD)', max_digits=15, decimal_places=2)
+
